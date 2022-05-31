8d9af241b015ce69dd67041d503ed24578d821f5
Abhinandan Jhamb <77092975+iam5abhi@users.noreply.github.com>
2022-05-31 12:13:20 +0530

    Add files via upload

--- details/src/page/getAccountDetails.js
+++ details/src/page/getAccountDetails.js
@@ -1,35 +1,109 @@
 import React from 'react'
-import './login.css'
+import './getAccountDetails.css'
 
 
-const Login =()=>{
+
+const GetAccountDetails =()=>{
     return(
         <>
-     
-        	<div class="login">
-            <div class=" login-key text-center">
-                  <i class="fa-solid fa-key" aria-hidden="true"></i>
-              </div>
-			<h1>LOGIN</h1>
-			<form action="/auth" method="post">
-				<label for="username">
+           <div className="container-fluid">
+                <div className='row'>
+                     <div class="col-6 border border-dark" >
+                     <div class="p-2 d-flex">
+                                        <div className='col-3'><b>Name</b></div>   
+                                        <div className='col-9'><b>************</b></div>         
+                                </div>
+                                <div class="p-2 d-flex">
+                                        <div className='col-3'><b>Address</b></div>
+                                        <div className='col-9'><b>************</b></div>
+                                </div>
+                     </div>
+                        <div class="col-6" >
+                                <div class="p-2 d-flex">
+                                        <div className='col-3'>Name</div>
+                                        <div className='col-9'>************</div>
+                                </div>
+                                <div class="p-2 d-flex">
+                                        <div className='col-3'>Branch</div>
+                                        <div className='col-9'>************</div>
+                                </div>
+                                <div class="p-2 d-flex">
+                                        <div className='col-3'>Account number</div>
+                                        <div className='col-9'>************</div>
+                                </div>
+                                <div class="p-2 d-flex">
+                                        <div className='col-3'>IFSC</div>
+                                        <div className='col-9'>************</div>
+                                </div>
+                                <div class="p-2 d-flex">
+                                        <div className='col-3'>Address</div>
+                                        <div className='col-9'>************</div>
+                                </div>
+                     </div>
+                </div>
+           </div>
+           <br></br>
+           <br></br>
+<h6 class="text-center"><b>Statement of Account</b></h6><br></br>
 
-					<i class="fas fa-user"></i>
-				</label>
-				<input type="text" name="username" placeholder="Username" id="username" required/>
-				<label for="password">
-					<i class="fas fa-lock"></i>
-				</label>
-				<input type="password" name="password" placeholder="Password" id="password" required/>
-				<input type="submit" value="Login"/>
-			</form>
-		</div>
+<div class="row">
+    <div class="col-6" ><b>From:-</b>08-06-2022</div>
+    <div class="col-6 " ><b>To:-</b>08-06-2022</div>
+</div>
+           
+  <table class="table table-striped">
+  <thead class="thead bg-dark text-white">
+    <tr>
+      <th scope="col">Date</th>
+      <th scope="col">Effective Date</th>
+      <th scope="col">Cheque/Sequence Number</th>
+      <th scope="col">Branch</th>
+      <th scope="col">Description</th>
+      <th scope="col">Withdrawal Amount</th>
+      <th scope="col">Deposit Amount</th>
+      <th scope="col">Balance</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+   
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
     
-
+    </tr>
+  </tbody>
+</table>
 
 
         </>
     )
 }
 
-export default Login
+export default GetAccountDetails
